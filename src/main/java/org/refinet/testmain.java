9f17672f136c47e2da130e23eeddc4216e02996e
CLEVO Computer <CLEVO Computer@DESKTOP-2CP6NLL.Speedport_W_724V_01011603_06_003>
2019-08-21 15:33:06 +0200

    Initial Commit

--- src/main/java/org/refinet/testmain.java
+++ src/main/java/org/refinet/testmain.java
@@ -1,13 +1,72 @@
 package org.refinet;
 
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.junit.jupiter.api.Test;
 import org.refinet.parser.JUnitTestParser;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+import com.github.javaparser.ast.comments.JavadocComment;
+import com.google.j2objc.annotations.ReflectionSupport;
+
 
 public class testmain {
 
 	public static void main(String[] args) {
-	JUnitTestParser.parse(new File("src/main/java/org/refinet/tests/CalculatorTests.java"));
 	
+
+    Reflections reflections = new Reflections("org.refinet.tests", new SubTypesScanner(false));
+    
+    Set<Class<? extends Object>> classes = 
+        reflections.getSubTypesOf(Object.class);
+    
+  
+    Object[] array = classes.toArray();
+    
+    
+    for (int i = 0; i < array.length; i++) {
+    	array[i] = "src/main/java/" + array[i].toString().substring(6, array[i].toString().length()).replace(".", "/") + ".java";
+    	
+	}
+    
+    for (int j = 0; j < array.length; j++) {
+    	if (array[j].toString().split("tests.java") != null ) {
+    	JUnitTestParser.parse(new File(array[j].toString()));
+    	System.out.println(array[j]);
+    	}else {
+    	}
+	}
+    
+    
+    
+  //JUnitTestParser.parse(new File(array[i].toString()));
+  
+   // JUnitTestParser.parse(new File(name)); 
+   // "src/main/java/org/refinet/tests/CalculatorTests.java"
+    //System.out.println(name);
+    
+
+
 	// JFileChooser-Objekt erstellen	
     //JFileChooser chooser = new JFileChooser();
     //int file = chooser.showOpenDialog(null);
@@ -17,5 +76,8 @@ public class testmain {
 	//JUnitTestParser.parse(new File(a));
     //}
 	}
+    
+  
+
 
 }
---
+++ src/main/java/org/refinet/tests/AccountingTests.java
@@ -0,0 +1,53 @@
+package org.refinet.tests;
+
+import org.junit.jupiter.api.Test;
+import org.refinet.steps.AccountingApp;
+import org.refinet.steps.CalculatorApp;
+import org.refinet.steps.CalculatorUsage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
+
+@DisplayName("Test our AccountingSystem")
+public class AccountingTests {
+  /**
+   * Open the Accounting app for testing.
+   */
+  @BeforeAll
+  public void loadAccounting() {
+    System.out.println("Some dummy debug information");
+    AccountingApp.open();
+  }
+  
+  /**
+   * 
+   * Clear input fields 
+   * 
+   */
+  @AfterEach
+  public void ClearAccouting() {
+	  AccountingApp.clear();
+  }
+
+  @AfterAll
+  @DisplayName("After we finished all the tests, we close our accouting app")
+  public void terminateAccounting() {
+    System.out.println("Some dummy debug information");
+    AccountingApp.close();
+  }
+
+}
+
+	
+
+
+
+
